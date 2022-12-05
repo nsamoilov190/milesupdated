99c7aff75dc3c47d49a5a3d26cd5baeb76067233
Nikita Samoilov <nikisamoylov@gmail.com>
2022-12-05 16:10:25 +0400

    init

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService calcBmi = mew BmiService();
+        double bmi = calcBmi.bmi(1.7, 88);
+        System.out.println(bmi);
+
+    }
+
+}
